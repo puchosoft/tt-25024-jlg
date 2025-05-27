4cb50f46b0f65a1b60770dc56dcd762ac2cac410
Jose Luis Gonzalez <puchosoft@gmail.com>
2025-05-26 22:11:32 -0300

    Requerimiento #1: Configuracion Inicial

---
+++ index.js
@@ -0,0 +1,10 @@
+// +==================================+
+// | Talento Tech - C#25024 - Node.JS |
+// +==================================+
+
+// +------------------------------+
+// | Proyecto Final (pre-entrega) |
+// |     Jose Luis Gonzalez       |
+// +------------------------------+
+
+console.log("Test de funcionamiento del script 'start'\n");
